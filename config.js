a8d5ce9b997ad276d9ac16e564aa20e7bf5fe402
AkshayBasti <70245648+AkshayBasti123@users.noreply.github.com>
2025-09-01 02:01:59 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   NEWS_API_KEY: "9ab277b731a1413092fcea1644db983f",     
   ALPHA_VANTAGE_KEY: "UHV4STGFZJQCSZRI",                  
-  HF_API_KEY: "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",    
+  HF_API_KEY: "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",    
   FMP_API_KEY: "demo"                                     
 };
