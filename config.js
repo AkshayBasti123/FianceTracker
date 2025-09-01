3bd025914b47763fccab368d470806b3973dba9e
AkshayBasti <70245648+AkshayBasti123@users.noreply.github.com>
2025-09-01 02:43:25 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ const CONFIG = {
   ALPHA_VANTAGE_KEY: "UHV4STGFZJQCSZRI",               // Stocks
   HF_API_KEY: "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",  // Hugging Face (read)
   FMP_API_KEY: "demo"                                   // FinancialModelingPrep news (demo works)
+  GNEWS_KEY: "8be6e2c560a3f8dd118abff88eb9c8b8"
 };
